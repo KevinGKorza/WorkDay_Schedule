c5cdabdeac74e3d983301dc5d28fdcd97c7ad2fd
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-08-02 22:08:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
+//Remember to display today's date and day
+//Remember camel letters
 
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+
+$(document).ready(function () {
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+})
+
+function timeTracker() {
+var currentTime = moment().hour();
+$(".time-block").each(function () {
+    var blockscheduleTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (blockscheduleTime < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (blockscheduleTime === currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+
+})
+
+}
+
+//local storage
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+
+timeTracker();
+
+})
