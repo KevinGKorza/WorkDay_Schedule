6cd1e46f5b4a19998229ff1718b7a54f479e8809
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-08-04 13:46:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 //Remember to display today's date and day
 //Remember camel letters
+//Use W3 schools, momentjs.com, class information/documentation, JQuery, and Bootstrap for help
 
+//Today's date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 
 $(document).ready(function () {
 $(".saveBtn").on("click", function () {
+    //Used the JQuery documentation for the values
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-    localStorage.setItem(time, text);
+    localStorage.setItem(time, text); //this will let the user save the time and text information in the local storage
 })
 
 function timeTracker() {
 var currentTime = moment().hour();
-$(".time-block").each(function () {
+$(".time-block").each(function () { //Time block loops, makes the user know when it's able to schedule an appontment, won't be able to schedule anything if it's in the past (gray), only present (red) and future (green) will be applicable 
     var blockscheduleTime = parseInt($(this).attr("id").split("hour")[1]);
 
     if (blockscheduleTime < currentTime) {
@@ -37,7 +40,7 @@ $(".time-block").each(function () {
 
 }
 
-//local storage
+//local storage for the schedule, you could make the appointment in the text area and save it and come back to it and it'll still be there 
 $("#hour8am .description").val(localStorage.getItem("hour8am"));
 $("#hour9am .description").val(localStorage.getItem("hour9am"));
 $("#hour10am .description").val(localStorage.getItem("hour10am"));
